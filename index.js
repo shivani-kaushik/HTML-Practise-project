1d1f19fb7afa42e5df9ef66e9d4adc16d181bf7e
Shivani Kaushik <shivani.kaushik@live.in>
2020-09-23 12:21:11 +0530

    this keyword

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var shivam = {
-  name: "shivam kaushik",
-  height: "6ft",
-  weight: "70kgs",
-  hobbies: ["eating", "sleeping", "dancing"],
-  isHungryAlways: true
+var dreamhome = {
+  location: "sainik farm",
+  type: "farm house",
+  area: "5000sqft",
+  amenities: ["garden", "dogs", "big house"],
+  mustHavePool: true,
+  greeting: "Hello Friend!",
+  sayGreeting: function () {
+    console.log(this.greeting)
+  }
 }
 
-console.log(shivam)
+console.log(dreamhome)
+
+dreamhome.sayGreeting()
+
+console.log(dreamhome.area)
